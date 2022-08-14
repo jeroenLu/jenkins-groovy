93aac1aa93b8b594772787b3606b9a7bc7a5665a
jeroenLu <jeroenluers@gmail.com>
2022-08-14 15:11:12 +0200

    print

--- vars/apiPipeline.groovy
+++ vars/apiPipeline.groovy
@@ -5,20 +5,27 @@ def call(String name = 'bla') {
     echo "In Call"
     def sequentialStages = []
 
-//    pipeline {
-//
-//        stages {
-//            stage('Prepare pipeline') {
-//                steps {
-//                    script {
-//                        echo "Adding stages to sequential build"
+    pipeline {
+        agent any
+
+        tools {
+            // Install the Maven version configured as "M3" and add it to the path.
+            maven "M3"
+            jdk 'openjdk-11'
+        }
+
+        stages {
+            stage('Prepare pipeline') {
+                steps {
+                    script {
+                        echo "Adding stages to sequential build"
 //
 //                        sequentialStages.addAll(pipelineComponentBuild())
 //
 //                        sequentialStages.each { it.call() }
-//                    }
-//                }
-//            }
-//        }
-//    }
+                    }
+                }
+            }
+        }
+    }
 }
