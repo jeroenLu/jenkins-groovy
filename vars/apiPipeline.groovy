5a377eafa902697705f175dda17f01c05f39c7bd
Jeroen Luers <jeroenluers@gmail.com>
2022-12-27 23:34:32 +0100

    Update apiPipeline.groovy

--- vars/apiPipeline.groovy
+++ vars/apiPipeline.groovy
@@ -13,7 +13,7 @@ def call(Map callParams) {
         tools {
             // Install the Maven version configured as "M3" and add it to the path.
             maven "M3"
-            jdk 'openjdk-11'
+            jdk 'jdk11'
         }
 
         environment  {
