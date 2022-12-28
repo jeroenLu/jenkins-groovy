12fe14160b71e0dcf1b671d8fdd0e5c543a12b3f
Jeroen Luers <jeroenluers@gmail.com>
2022-12-28 01:17:48 +0100

    Update pipelineVersionUpdate.groovy

--- vars/pipelineVersionUpdate.groovy
+++ vars/pipelineVersionUpdate.groovy
@@ -41,7 +41,7 @@ void commitVersion() {
          sh 'git config user.email jeroenluers@gmail.com'
          sh "git add ."
          sh 'git commit -m pomUpdate --allow-empty'
-         sh 'git push HEAD:master'
+         sh 'git push HEAD:origin master'
     }
     
 //    sshagent(credentials: ['GIT_SA_SSH']) {
